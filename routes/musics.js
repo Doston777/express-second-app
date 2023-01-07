5b6037a6c3355fbdbc832b831c9ca9a6ee0fd914
Doston777 <dmamadaminov7@gmail.com>
2023-01-07 12:59:33 +0500

    Update and create musics db

--- routes/musics.js
+++ routes/musics.js
@@ -14,11 +14,34 @@ router.post('/', (req, res) => {
   })
 })
 
-/* GET users listing. */
-router.get("/", (req, res, next) => {
-  Music.find({}, (err, music) => {
-    err ? res.status(err.name) : res.status(200).json(music)
+// Get music by id
+router.get("/:id", (req, res) => {
+  Music.findById(req.params.id, (err, music) => {
+    err ? res.status(404) : res.status(200).json(music)
   })
 })
+
+// Update music by id
+router.put("/:id", (req, res) => {
+  Music.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    {
+      new: true
+    },
+    (err, music) => {
+      err ? res.json(err) : res.status(204).json(music)
+    }
+  )
+})
+
+
+// Get all users
+router.get("/", (req, res, next) => {
+  Music.find({}, (err, music) => {
+    err ? res.status(404) : res.status(200).json(music)
+  })
+})
+
 
 module.exports = router
